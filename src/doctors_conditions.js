f7e8ce320c3687804d68977f3e45016bb588ccea
Miles Bradt <smbradtmichael@gmail.com>
2019-01-18 12:02:09 -0800

    update promise to find doctors based off symptoms

--- src/doctors_conditions.js
+++ src/doctors_conditions.js
@@ -1,9 +1,10 @@
 export class DoctorConditions {
-  getDoctorsForCondition() {
+  getDoctorsForCondition(symptom) {
+    let apiKey = exports.apiKey;
+    let userSymptom = symptom;
     return new Promise(function(resolve, reject) {
-      let apiKey = exports.apiKey;
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=' + apiKey;
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=' + userSymptom + '&location=45.512%2C-122.658%2C100&user_location=45.512%2C-122.658&skip=0&limit=10&user_key=4a9d02a663b191206be1f400d948e89c' + apiKey;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
