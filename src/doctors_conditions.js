f2eda25dc8e377b207c88b86d4773105be0aa6f3
Miles Bradt <smbradtmichael@gmail.com>
2019-01-18 12:12:34 -0800

    main for using promise, API key is undefined

--- src/doctors_conditions.js
+++ src/doctors_conditions.js
@@ -1,10 +1,10 @@
 export class DoctorConditions {
   getDoctorsForCondition(symptom) {
-    let apiKey = exports.apiKey;
+    let apiKey = process.env.apiKey;
     let userSymptom = symptom;
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=' + userSymptom + '&location=45.512%2C-122.658%2C100&user_location=45.512%2C-122.658&skip=0&limit=10&user_key=4a9d02a663b191206be1f400d948e89c' + apiKey;
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=' + userSymptom + '&location=45.512%2C-122.658%2C100&user_location=45.512%2C-122.658&skip=0&limit=10&user_key=' + apiKey;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-// import { Class } from './class.js';
+import { DoctorConditions } from './doctors_conditions.js';
+
+
+$(document).ready(function() {
+  let doctorPromiseAPI = new DoctorConditions();
+  let doctorSearch = doctorPromiseAPI.getDoctorsForCondition("acne");
+  doctorSearch.then(function(response) {
+    let body = JSON.parse(response);
+
+    console.log(body);
+  });
+
+});
