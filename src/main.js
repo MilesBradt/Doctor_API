b06880629278f774323f7446540524082f11c042
Miles Bradt <smbradtmichael@gmail.com>
2019-01-18 14:32:22 -0800

    show name, address, number, and if accepting new patients

--- src/main.js
+++ src/main.js
@@ -12,7 +12,9 @@ $(document).ready(function() {
     let body = JSON.parse(response);
 
     for (let i = 0; i < body.data.length; i++) {
-      console.log(body.data[i].profile);
+      console.log(body.data[i]);
+      console.log("First Name:", body.data[i].profile.first_name, ",", "Last Name:", body.data[i].profile.last_name);
+      console.log(body.data[i].practices[0].name, body.data[i].practices[0].visit_address.city, body.data[i].practices[0].visit_address.state, body.data[i].practices[0].visit_address.street, body.data[i].practices[0].visit_address.street2, body.data[i].practices[0].visit_address.zip, body.data[i].practices[0].phones[0].number, body.data[i].practices[0].phones[0].type, body.data[i].practices[0].accepts_new_patients);
     }
   });
 
