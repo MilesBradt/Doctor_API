1e8f456349b3632828069a6718fb379c19a7b170
Miles Bradt <smbradtmichael@gmail.com>
2019-01-18 16:28:35 -0800

    correct information to DOM

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import { DoctorByName } from './doctor_names.js';
 
 
 $(document).ready(function() {
-  $("#buttonSubmit").click(function() {
+  $("#buttonSubmitSymptom").click(function() {
     document.getElementById("output").innerHTML = '';
-    let search = $("input#userSearch").val();
+    let search = $("input#userSearchSymptom").val();
     console.log(search);
     let doctorPromiseByCondition = new DoctorConditions();
     let doctorSearch = doctorPromiseByCondition.getDoctorsForCondition(search);
@@ -22,28 +22,35 @@ $(document).ready(function() {
         // }
 
         console.log(body.data[i]);
-        $("#output").append("<li>" + "Name: " + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "<br>" + "Address: " + body.data[i].practices[0].name + ", " + body.data[i].practices[0].visit_address.city + ", " + body.data[i].practices[0].visit_address.state + " " + body.data[i].practices[0].visit_address.zip + "</li>")
+        $("#output").append("<li>" + "Name: " + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "<br>" + "Address: " + body.data[i].practices[0].name + " " + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.city + ", " + body.data[i].practices[0].visit_address.state + " " + body.data[i].practices[0].visit_address.zip + "<br>" + "Number: " + body.data[i].practices[0].phones[0].number + "<br>" + "Accepting new patients? " + body.data[i].practices[0].accepts_new_patients + "</li>")
+
+
         console.log(body.data[i].practices[0].name, body.data[i].practices[0].visit_address.city, body.data[i].practices[0].visit_address.state, body.data[i].practices[0].visit_address.street, body.data[i].practices[0].visit_address.street2, body.data[i].practices[0].visit_address.zip, body.data[i].practices[0].phones[0].number, body.data[i].practices[0].phones[0].type, body.data[i].practices[0].accepts_new_patients);
       }
     });
   });
-  // let doctorPromiseByName = new DoctorByName();
-  // let doctorSearchName = doctorPromiseByName.getDoctorsByName("Michael");
-  //
-  // doctorSearchName.then(function(response) {
-  //   let body = JSON.parse(response);
-  //
-  //   for (let i = 0; i < body.data.length; i++) {
-  //     console.log(body.data[i]);
-  //     console.log("First Name:", body.data[i].profile.first_name, ",", "Last Name:", body.data[i].profile.last_name);
-  //     console.log(body.data[i].practices[0].name, body.data[i].practices[0].visit_address.city, body.data[i].practices[0].visit_address.state, body.data[i].practices[0].visit_address.street, body.data[i].practices[0].visit_address.street2, body.data[i].practices[0].visit_address.zip, body.data[i].practices[0].phones[0].number, body.data[i].practices[0].phones[0].type, body.data[i].practices[0].accepts_new_patients);
-  //     let phones = body.data[i].practices[0].phones
-  //     console.log(phones);
-  //     // for (let j = 0; i < phones.length; j++) {
-  //
-  //   }
-  // });
+
+  $("#buttonSubmitName").click(function() {
+    document.getElementById("output").innerHTML = '';
+    let search = $("input#userSearchName").val();
+    let doctorPromiseByName = new DoctorByName();
+    let doctorSearchName = doctorPromiseByName.getDoctorsByName(search);
+
+    doctorSearchName.then(function(response) {
+      let body = JSON.parse(response);
+
+      for (let i = 0; i < body.data.length; i++) {
+        // phones.forEach(phone) {
+        //   console.log(phone.number, phone.type)
+        // }
+
+        console.log(body.data[i]);
+        $("#output").append("<li>" + "Name: " + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "<br>" + "Address: " + body.data[i].practices[0].name + " " + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.city + ", " + body.data[i].practices[0].visit_address.state + " " + body.data[i].practices[0].visit_address.zip + "<br>" + "Number: " + body.data[i].practices[0].phones[0].number + "<br>" + "Accepting new patients? " + body.data[i].practices[0].accepts_new_patients + "</li>")
 
 
+        console.log(body.data[i].practices[0].name, body.data[i].practices[0].visit_address.city, body.data[i].practices[0].visit_address.state, body.data[i].practices[0].visit_address.street, body.data[i].practices[0].visit_address.street2, body.data[i].practices[0].visit_address.zip, body.data[i].practices[0].phones[0].number, body.data[i].practices[0].phones[0].type, body.data[i].practices[0].accepts_new_patients);
+      }
+    });
+  });
 
 });
